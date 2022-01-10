e13e12cc83a4c78e7444bfe7b8563c690590f767
heyallKathere <97472035+heyallKathere@users.noreply.github.com>
2022-01-10 14:26:03 -0500

    Update Game.py

--- UNO/Game.py
+++ UNO/Game.py
@@ -57,7 +57,7 @@ def draw_text(text, font, text_col, x, y):
     
 #class made by kat
 class deck():
-    int = random.randrange(0,2)
+    int = random.randrange(0,14)
     deckcards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "Draw two", "Reverse Card", "Skip Card", "Colour Wheel", "Pick up 4"]
     colours = ["Red", "Green", "Yellow", "Blue"]
     
