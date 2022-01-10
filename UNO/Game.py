54507143d5792c700ca8fc5ae2265ba306b94725
heyallKathere <97472035+heyallKathere@users.noreply.github.com>
2022-01-10 14:12:31 -0500

    Update Game.py

--- UNO/Game.py
+++ UNO/Game.py
@@ -58,9 +58,7 @@ def draw_text(text, font, text_col, x, y):
 class deck():
    
         colours = { "Red", "Green", "Yellow", "Blue"}
-        numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-        action cards = {"Draw two", "Reverse Card", "Skip Card"}
-        wild cards = { "Colour Wheel", "Pick up 4"}
+        deckcards = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "Draw two", "Reverse Card", "Skip Card", "Colour Wheel", "Pick up 4"}
         
 
 class Button():
