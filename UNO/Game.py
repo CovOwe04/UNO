32de7ecb2887e1df6a1c2f1ec03eb9410e900cd9
heyallKathere <97472035+heyallKathere@users.noreply.github.com>
2022-01-10 14:36:59 -0500

    Update Game.py

--- UNO/Game.py
+++ UNO/Game.py
@@ -56,7 +56,7 @@ def draw_text(text, font, text_col, x, y):
     screen.blit(img, (x, y))
     
 #class made by kat
-class deck():
+class deck(self):
     numcards = 108
     deckcards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "Draw two", "Reverse Card", "Skip Card", "Colour Wheel", "Pick up 4"]
     colours = ["Red", "Green", "Yellow", "Blue"]
