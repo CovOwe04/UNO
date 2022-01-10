04b6d946d61ecc762fc0b72630c2347254391002
heyallKathere <97472035+heyallKathere@users.noreply.github.com>
2022-01-10 15:51:29 -0500

    Update Game.py

--- UNO/Game.py
+++ UNO/Game.py
@@ -10,8 +10,8 @@ import random
 pygame.init()
 
 #setting a pygame clock variable to set a framerate for the game
-#fps = Frames per second
 clock = pygame.time.Clock()
+#fps = Frames per second
 fps = 60
 
 #creating a screen width and height variables, width is 1000 and height is 600
@@ -57,7 +57,7 @@ def draw_text(text, font, text_col, x, y):
     #draws the text on the display window
     screen.blit(img, (x, y))
     
-#class made by kat
+#class made by Kat :)
 # creating a class for the card deck uno
 class deck(self):
     def__init__(self):
@@ -83,33 +83,33 @@ class deck(self):
                         self.drawcard.append(self.deckcards[int])
     
     
-        
+# creates a class for the button in regards to the location, size and if it is clicked or not        
 class Button():
     #this function is the creation of the button itself, its called when the class is called
     def __init__(self, x, y, image):
         #sets the button's image to the given image
         self.image = image
 
-        #sets a rectangle to be the size of the image size(width x height). used for collision
+        #sets a rectangle to be the size of the image size(width x height)
         self.rect = self.image.get_rect()
 
-        #sets the x and y coordinates of the rectangle to the given points
+        #sets the x and y coordinates of the rectangle to the given vlaues on lines 140 - 142
         self.rect.x = x
         self.rect.y = y
 
-        #setting a variable to check if a button is clicked
+        #setting a variable to check if a button is clicked or not
         self.clicked = False
    
     #this function checks for collision as well as draws the button onto the window
     def draw(self, image, image2):
 
-        #creating a variable for returning an action by the user to the main program
+        #creating a variable for returning a action by the mouse by the user to the main program
         action = False
 
-        #gets mouse position, sets it to a variable
+        #gets the mouse position, sets it to a variable named pos
         pos = pygame.mouse.get_pos()
 
-        #checks if the mouse is over the button
+        #checks if the mouse is hovering over the button
         if self.rect.collidepoint(pos):
             #if the mouse is on the button, set the image to the hover image
             self.image = image 
@@ -121,7 +121,7 @@ class Button():
                 action = True
                 self.clicked = True
         
-        #if mouse if not over button, set the image to the original button image
+        #if mouse if not over button, set the image to the original button image unaltered
         else:
             self.image = image2        
 
@@ -131,7 +131,7 @@ class Button():
             self.clicked = False
         
 
-        #draw button
+        #drawing the button
         screen.blit(self.image, self.rect)
 
         return action
@@ -141,23 +141,24 @@ Singleplayer_btn = Button(330, 270, singleplayer_img)
 Multiplayer_btn = Button(330, 400, multiplayer_img)
 Play_btn = Button(330, 450, play_img)
 
-#starts the game loop
+#starts the main game loop
 run = True
 while run == True:
 
     #runs this if statement if the gameplay variable is 0 (start of program or game over)
     if Gameplay == 0:
-        #prints the background first before the other images
+        #prints the background first before the other images so there is no overlap
         screen.blit(pygame.transform.scale(bg_img,(screen_width,screen_height)), (0,0))
 
         #sets the main menu and instructions variable to true to make them reappear
         main_menu = True
         instructions = True
 
-    #runs this if statement if the gameplay variable is 1 (player selected singleplayer)
+    #runs this if statement if the gameplay variable is 1 (player selected singleplayer mode)
     elif Gameplay == 1:
-        #prints the background first before the other images
+        #prints the background first before the other images so there is no overlap
         screen.blit(pygame.transform.scale(bg_img,(screen_width,screen_height)), (0,0))
+        #something here
         if instructions == True:
             draw_text('Instructions', font, White, 420, 10)
             if Play_btn.draw(play_img_hover, play_img):
@@ -168,7 +169,7 @@ while run == True:
     #runs this if statement if the gameplay variable is 2 (player selected singleplayer)
     elif Gameplay == 2:
 
-        #prints the background first before the other images
+        #prints the background first before the other images so there is no overlap
         screen.blit(pygame.transform.scale(bg_img,(screen_width,screen_height)), (0,0))
 
         #checks if the instructions variable is true
@@ -183,14 +184,14 @@ while run == True:
                 #sets the instructions variable as false
                 instructions = False
 
-                #delays button reaction for 100 milliseconds
+                #delays button reaction for 100 milliseconds so it is not instatanous 
                 pygame.time.delay(100)
 
-                #
+                #put something here
                 screen.blit(pygame.transform.scale(bg_img,(screen_width,screen_height)), (0,0))
-                #start coding here
+                #start coding here for multiplayer
                 
-
+    #put something here
     if main_menu == True:
         screen.blit(pygame.transform.scale(title_img,(320,225)), (340,40))
         if Singleplayer_btn.draw(singleplayer_img_hover, singleplayer_img):
@@ -198,19 +199,21 @@ while run == True:
             Gameplay = 1
             pygame.time.delay(100)
             screen.blit(pygame.transform.scale(bg_img,(screen_width,screen_height)), (0,0))
-
+            
+        #put something here
         if Multiplayer_btn.draw(multiplayer_img_hover, multiplayer_img):
             main_menu = False
             Gameplay = 2
             pygame.time.delay(100)
             screen.blit(pygame.transform.scale(bg_img,(screen_width,screen_height)), (0,0))
-
+            
+    # if the player has quit the game then the program with close
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     
     clock.tick(fps)
-    #updates the pygame display
+    #updates the pygame display after each user input
     pygame.display.update()
     
 # closes the pygame application
